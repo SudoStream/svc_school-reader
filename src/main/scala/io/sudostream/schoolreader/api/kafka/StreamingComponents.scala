12d2975d43ce0495c73f6e78ff79c16ec113761e
Andy Boyle <andy@sudostream.io>
2018-01-09 16:15:30 +0000

    0.0.1-9 : Add SASL JAAS Config to service

--- src/main/scala/io/sudostream/schoolreader/api/kafka/StreamingComponents.scala
+++ src/main/scala/io/sudostream/schoolreader/api/kafka/StreamingComponents.scala
@@ -1,10 +1,14 @@
 package io.sudostream.schoolreader.api.kafka
 
 import akka.actor.ActorSystem
+import akka.event.LoggingAdapter
 import akka.kafka.ProducerSettings
 import akka.stream.Materializer
-import io.sudostream.timetoteach.kafka.serializing.SystemEventSerializer
 import io.sudostream.schoolreader.config.{ActorSystemWrapper, ConfigHelper}
+import io.sudostream.timetoteach.kafka.serializing.SystemEventSerializer
+import io.sudostream.timetoteach.messages.events.SystemEvent
+import org.apache.kafka.clients.CommonClientConfigs
+import org.apache.kafka.common.config.SaslConfigs
 import org.apache.kafka.common.serialization.ByteArraySerializer
 
 import scala.concurrent.ExecutionContextExecutor
@@ -13,17 +17,25 @@ class StreamingComponents(configHelper: ConfigHelper, actorSystemWrapper: ActorS
   implicit val system: ActorSystem = actorSystemWrapper.system
   implicit val executor: ExecutionContextExecutor = system.dispatcher
   implicit val materializer: Materializer = actorSystemWrapper.materializer
-  val log = system.log
+  val log: LoggingAdapter = system.log
 
-  lazy val kafkaProducerBootServers = configHelper.config.getString("akka.kafka.producer.bootstrapservers")
+  lazy val kafkaProducerBootServers: String = configHelper.config.getString("akka.kafka.producer.bootstrapservers")
+  lazy val kafkaSaslJaasUsername: String = configHelper.config.getString("akka.kafka.saslJassUsername")
+  lazy val kafkaSaslJaasPassword: String = configHelper.config.getString("akka.kafka.saslJassPassword")
+  lazy val kafkaSaslJaasConfig: String = s"org.apache.kafka.common.security.scram.ScramLoginModule required " +
+    s"""username="$kafkaSaslJaasUsername" password="$kafkaSaslJaasPassword";"""
 
-  val producerSettings = ProducerSettings(system, new ByteArraySerializer, new SystemEventSerializer)
-    .withBootstrapServers(kafkaProducerBootServers)
+  val producerSettings: ProducerSettings[Array[Byte], SystemEvent] =
+    ProducerSettings(system, new ByteArraySerializer, new SystemEventSerializer)
+      .withBootstrapServers(kafkaProducerBootServers)
+      .withProperty(SaslConfigs.SASL_JAAS_CONFIG, kafkaSaslJaasConfig)
+      .withProperty(SaslConfigs.SASL_MECHANISM, "SCRAM-SHA-256")
+      .withProperty(CommonClientConfigs.SECURITY_PROTOCOL_CONFIG, "SASL_SSL")
+
+  log.info(s"Systems event topic is '$definedSystemEventsTopic'")
 
   def definedSystemEventsTopic: String = {
-    val sink_topic = configHelper.config.getString("school-reader.system_events_topic")
-    log.info(s"Sink topic is '$sink_topic'")
-    sink_topic
+    configHelper.config.getString("school-reader.system_events_topic")
   }
 
 }
